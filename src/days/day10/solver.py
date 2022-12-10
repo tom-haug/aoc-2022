eacbee3ed4ac26247a4e56fc0cf51e38755e8375
Tom Haug <tomhaug75@gmail.com>
2022-12-10 08:33:08 -0500

    separate computer

--- src/days/day10/solver.py
+++ src/days/day10/solver.py
@@ -1,58 +1,12 @@
 from __future__ import annotations
 from abc import abstractmethod
 from copy import copy
-from enum import Enum
+from src.shared.computer import Instruction, Processor, Registers
 from src.shared.controller import Solver
 from src.shared.file_loading import load_text_file_lines
 
 
 AnswerType = int | str
-Registers = dict[str, int]
-
-
-class InstructionType(Enum):
-    UNKNOWN = 0
-    NOOP = 1
-    ADDX = 2
-
-    @classmethod
-    def parse(cls, input: str) -> InstructionType:
-        match input:
-            case "noop":
-                return cls.NOOP
-            case "addx":
-                return cls.ADDX
-            case _:
-                return cls.UNKNOWN
-
-
-class Instruction:
-    type: InstructionType
-    value: int
-
-    def __init__(self, input: str):
-        parts = input.split(" ")
-        self.type = InstructionType.parse(parts[0])
-        self.value = int(parts[1]) if len(parts) > 1 else 0
-
-
-class Processor:
-    registers: Registers
-
-    def __init__(self, *register_keys: str):
-        self.registers = {}
-        for key in register_keys:
-            self.registers[key] = 1
-
-    def execute(self, instr: Instruction) -> int:
-        match instr.type:
-            case InstructionType.NOOP:
-                return 1
-            case InstructionType.ADDX:
-                self.registers["X"] += instr.value
-                return 2
-            case _:
-                return 0
 
 
 class Day10Solver(Solver[AnswerType]):
